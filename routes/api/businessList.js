e3fad6165c52ee6b35d8f946bf07825628e57732
unknown <mikesdatascience@yahoo.com>
2020-02-17 21:56:40 -0500

    dwd :

--- routes/api/businessList.js
+++ routes/api/businessList.js
@@ -5,26 +5,19 @@ const Business = require("../../models/Business");
 const authUser = require("../../middleware/authUser");
 
 router.post("/businessSearch", authUser, async (req, res) => {
-  // state
-  // zip
-  // city
-  // state zip
-  // state city
-  // state zip city
-  // city zip
   if (
     req.body.state !== "" &&
     req.body.zip === "" &&
     req.body.city === "" &&
     req.body.businessName === ""
   ) {
-    let businessesByState = await Business.find({ state: req.body.state });
+    let businessesByState = await Business.find({ state: req.body.state }).select(['schedule', '_id', 'businessName', 'address', 'city', 'zip', 'website', 'phoneNumber']);
     if (businessesByState.length > 0) {
       let businessAndProfileState = [];
       for (let i = 0; i < businessesByState.length; i++) {
         let businessProfileFound = await BusinessProfile.findOne({
           business: businessesByState[i]._id
-        });
+        }).select(['serviceTypes']);
         if (businessProfileFound) {
           businessAndProfileState.push({
             business: businessesByState[i],
@@ -34,7 +27,7 @@ router.post("/businessSearch", authUser, async (req, res) => {
       }
       return res.json({ businessesBack: businessAndProfileState });
     } else {
-      let allBusinesses = await Business.find({});
+      let allBusinesses = await Business.find({}).select(['schedule', '_id', 'businessName', 'address', 'city', 'zip', 'website', 'phoneNumber', 'state']);
       let foundFromPartialStateName = [];
       for (let i = 0; i < allBusinesses.length; i++) {
         if (allBusinesses[i].state.toLowerCase().includes(req.body.state.toLowerCase())) {
@@ -46,7 +39,7 @@ router.post("/businessSearch", authUser, async (req, res) => {
         for (let e = 0; e < foundFromPartialStateName.length; e++) {
           let profile = await BusinessProfile.findOne({
             business: foundFromPartialStateName[e]._id
-          });
+          }).select(['serviceTypes']);
           profileAndBusinessFromPartialState.push({
             business: foundFromPartialStateName[e],
             profile
@@ -69,13 +62,13 @@ router.post("/businessSearch", authUser, async (req, res) => {
     req.body.city === "" &&
     req.body.businessName === ""
   ) {
-    let businessesByZip = await Business.find({ zip: req.body.zip });
+    let businessesByZip = await Business.find({ zip: req.body.zip }).select(['schedule', '_id', 'businessName', 'address', 'city', 'zip', 'website', 'phoneNumber', 'state']);
     if (businessesByZip.length > 0) {
       let businessesAndProfileZip = [];
       for (let i = 0; i < businessesByZip.length; i++) {
         let businessProfileFound = await BusinessProfile.findOne({
           business: businessesByZip[i]._id
-        });
+        }).select(['serviceTypes']);
         if (businessProfileFound) {
           businessesAndProfileZip.push({
             business: businessesByZip[i],
@@ -95,13 +88,13 @@ router.post("/businessSearch", authUser, async (req, res) => {
     req.body.city !== "" &&
     req.body.businessName === ""
   ) {
-    let businessesByCity = await Business.find({ city: req.body.city });
+    let businessesByCity = await Business.find({ city: req.body.city }).select(['schedule', '_id', 'businessName', 'address', 'city', 'zip', 'website', 'phoneNumber', 'state']);
     if (businessesByCity.length > 0) {
       let businessAndProfileCity = [];
       for (let i = 0; i < businessesByCity.length; i++) {
         let profileFound = await BusinessProfile.findOne({
           business: businessesByCity[i]._id
-        });
+        }).select(['serviceTypes']);
         if (profileFound) {
           businessAndProfileCity.push({
             business: businessesByCity[i],
@@ -111,7 +104,7 @@ router.post("/businessSearch", authUser, async (req, res) => {
       }
       return res.json({ businessesBack: businessAndProfileCity });
     } else {
-      let allBusinesses = await Business.find({});
+      let allBusinesses = await Business.find({}).select(['schedule', '_id', 'businessName', 'address', 'city', 'zip', 'website', 'phoneNumber', 'state']);;
       let businessesThatMatchCity = [];
       for (let i = 0; i < allBusinesses.length; i++) {
         if (allBusinesses[i].city.toLowerCase().includes(req.body.city.toLowerCase())) {
@@ -123,7 +116,7 @@ router.post("/businessSearch", authUser, async (req, res) => {
         for (let z = 0; z < businessesThatMatchCity.length; z++) {
           let businessProfileFound = await BusinessProfile.findOne({
             business: businessesThatMatchCity[z]._id
-          });
+          }).select(['serviceTypes']);
           if (businessProfileFound) {
             businessesAndProfile.push({
               business: businessesThatMatchCity[z],
@@ -143,13 +136,13 @@ router.post("/businessSearch", authUser, async (req, res) => {
   } else if (req.body.businessName !== "") {
     let businessesByBusinessName = await Business.find({
       businessName: req.body.businessName
-    });
+    }).select(['schedule', '_id', 'businessName', 'address', 'city', 'zip', 'website', 'phoneNumber', 'state']);;
     if (businessesByBusinessName.length > 0) {
       let businessesAndProfileBusinessName = [];
       for (let i = 0; i < businessesByBusinessName.length; i++) {
         let businessProfileFound = await BusinessProfile.findOne({
           business: businessesByBusinessName[i]._id
-        });
+        }).select(['serviceTypes']);
         if (businessProfileFound) {
           businessesAndProfileBusinessName.push({
             business: businessesByBusinessName[i],
@@ -171,13 +164,13 @@ router.post("/businessSearch", authUser, async (req, res) => {
     let businessesFromStateAndZip = await Business.find({
       state: req.body.state,
       zip: req.body.zip
-    });
+    }).select(['schedule', '_id', 'businessName', 'address', 'city', 'zip', 'website', 'phoneNumber', 'state']);;
     if (businessesFromStateAndZip.length > 0) {
       let businessAndProfileFromStateAndZip = [];
       for (let i = 0; i < businessesFromStateAndZip.length; i++) {
         let profileFromStateAndZip = await BusinessProfile.findOne({
           business: businessesFromStateAndZip[i]._id
-        });
+        }).select(['serviceTypes']);
         if (profileFromStateAndZip) {
           businessAndProfileFromStateAndZip.push({
             business: businessesFromStateAndZip[i],
@@ -203,13 +196,13 @@ router.post("/businessSearch", authUser, async (req, res) => {
     let businessesFromStateAndCity = await Business.find({
       state: req.body.state,
       city: req.body.city
-    });
+    }).select(['schedule', '_id', 'businessName', 'address', 'city', 'zip', 'website', 'phoneNumber', 'state']);
     if (businessesFromStateAndCity.length > 0) {
       let businessAndProfileFromStateAndCity = [];
       for (let i = 0; i < businessesFromStateAndCity.length; i++) {
         let profileFromStateAndCity = await BusinessProfile.findOne({
           business: businessesFromStateAndCity[i]
-        });
+        }).select(['serviceTypes']);
         if (profileFromStateAndCity) {
           businessAndProfileFromStateAndCity.push({
             business: businessesFromStateAndCity[i],
@@ -236,7 +229,7 @@ router.post("/businessSearch", authUser, async (req, res) => {
       state: req.body.state,
       city: req.body.city,
       zip: req.body.zip
-    });
+    }).select(['schedule', '_id', 'businessName', 'address', 'city', 'zip', 'website', 'phoneNumber', 'state']);
     if (businessesFromStateAndCityAndZip.length > 0) {
       let businessAndProfileFromStateAndCityAndZip = [];
       for (let i = 0; i < businessesFromStateAndCityAndZip.length; i++) {
@@ -268,7 +261,7 @@ router.post("/businessSearch", authUser, async (req, res) => {
     let businessesFromZipAndCity = await Business.find({
       zip: req.body.zip,
       city: req.body.city
-    });
+    }).select(['schedule', '_id', 'businessName', 'address', 'city', 'zip', 'website', 'phoneNumber', 'state']);
     if (businessesFromZipAndCity.length > 0) {
       let businessAndProfileFromZipAndCity = [];
       for (let i = 0; i < businessesFromZipAndCity.length; i++) {
